2b0ddaf2ac7f6a08785f85ab4dd2089e0cc37ab3
Aman Shahi <amanshahidev@gmail.com>
2026-01-20 20:52:32 +0530

    updated img path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,83 +5,83 @@ function Home() {
 
   const imagesArray = [
     {
-      src: '/1.jpg',
+      src: '/chainsmokers/1.jpg',
       title: 'image-1',
     },
     {
-      src: '/2.jpg',
+      src: '/chainsmokers/2.jpg',
       title: 'image-2',
     },
     {
-      src: '/3.jpg',
+      src: '/chainsmokers/3.jpg',
       title: 'image-3',
     },
     {
-      src: '/4.jpg',
+      src: '/chainsmokers/4.jpg',
       title: 'image-4',
     },
     {
-      src: '/5.jpg',
+      src: '/chainsmokers/5.jpg',
       title: 'image-5',
     },
     {
-      src: '/6.jpg',
+      src: '/chainsmokers/6.jpg',
       title: 'image-6',
     },
     {
-      src: '/7.jpg',
+      src: '/chainsmokers/7.jpg',
       title: 'image-7',
     },
     {
-      src: '/8.jpg',
+      src: '/chainsmokers/8.jpg',
       title: 'image-8',
     },
     {
-      src: '/9.jpg',
+      src: '/chainsmokers/9.jpg',
       title: 'image-9',
     },
     {
-      src: '/10.jpg',
+      src: '/chainsmokers/10.jpg',
       title: 'image-10',
     },
     {
-      src: '/11.jpg',
+      src: '/chainsmokers/11.jpg',
       title: 'image-11',
     },
     {
-      src: '/12.jpg',
+      src: '/chainsmokers/12.jpg',
       title: 'image-12',
     },
     {
-      src: '/13.jpg',
+      src: '/chainsmokers/13.jpg',
       title: 'image-13',
     },
     {
-      src: '/14.jpg',
+      src: '/chainsmokers/14.jpg',
       title: 'image-14',
     },
     {
-      src: '/15.jpg',
+      src: '/chainsmokers/15.jpg',
       title: 'image-15',
     },
     {
-      src: '/16.jpg',
+      src: '/chainsmokers/16.jpg',
       title: 'image-16',
     },
     {
-      src: '/17.jpg',
+      src: '/chainsmokers/17.jpg',
       title: 'image-17',
     },
     {
-      src: '/18.jpg',
+      src: '/chainsmokers/18.jpg',
       title: 'image-18',
     },
     {
-      src: '/19.jpg',
+      src: '/chainsmokers/19.jpg',
       title: 'image-19',
     },
     {
-      src: '/20.jpg',
+      src: '/chainsmokers/20.jpg',
       title: 'image-20',
     },
   ]
